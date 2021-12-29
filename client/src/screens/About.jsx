29f36065a04244ab2d0beedaf50fc41f86d5e97e
jckrft <jckrft@gmail.com>
2021-12-28 21:10:00 -0500

    links working and resume added

--- client/src/screens/About.jsx
+++ client/src/screens/About.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import resume from '../assets/files/resume.pdf'
 
 export default function About() {
   return (
     <div>
       <p>about</p>
+      <Link to='/contact'>contact</Link>
+      <a href={resume} target="_blank">resume</ a>
     </div>
   )
 }
