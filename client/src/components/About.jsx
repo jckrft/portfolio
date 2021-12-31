92377ee63c01ceaa2283c5471404596a4c7b375d
jckrft <jckrft@gmail.com>
2021-12-31 01:48:58 -0500

    lots of css

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import resume from "../assets/files/resume.pdf";
+import me from "../assets/images/me.png";
+import "../assets/css/About.css";
 
 export default function About() {
   return (
-    <div>
-      <p>Hi! I'm Jackie</p>
-      <a href={resume}
-        target="_blank"
-        rel="noreferrer">
-        resume
-      </a>
-
+    <div className="about-image">
+      {/* <div>
+        <img className="img-me" src={me} alt="me" />
+      </div> */}
+      <div className="about">
+        <p className="about-detail">
+          the U.S. government and my mother call me Jacquelyn, but it's a bit
+          formal, no? I'm an authentic and inventive Software Engineer. I once
+          opened a can with a spoon and sawed a toilet seat off with a serated
+          knife and a lighter, but really that's neither here nor there. Once I
+          discovered my love for coding, I enthusicatically immersed myself in
+          everything there is to know. I pride myself on my ability to
+          commuicate, especially virtually, due to my background in teaching
+          English. I'm able to work creatively under pressure and find out of
+          the box solutions. Being curious by nature, I love to learn and am
+          constantly researching new things. As a self-professed world traveler,
+          I bring adaptability and a thirst for new and challenging experiences
+          to my career, as well as my personal life. Tenacious and always up for
+          something new, bring it on!
+        </p>
+        <a href={resume} target="_blank" rel="noreferrer">
+          resume
+        </a>
+      </div>
     </div>
   );
 }
--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import flower from '../assets/images/flower.png'
+import "../assets/css/Landing.css";
+// import flower from '../assets/images/flower.png'
 
 export default function Landing() {
   return (
-    <div className='landing'>
+    <div className="landing-div">
+      <div className="landing">
+      <div className="greeting">
+        <h1 className="hi">hi!</h1>
+        <h1 className="im">i'm</h1>
+      </div>
+      <div className="name-description">
+      <h1 className="description">software engineer</h1>
+        <h1 className="jackie">jackie</h1>
+        </div>
+        </div>
       {/* <h1 className='name'>jacquelyn kraft</h1> */}
       {/* <img
         className='flower'
         src={flower} alt='flower' /> */}
     </div>
-  )
+  );
 }
