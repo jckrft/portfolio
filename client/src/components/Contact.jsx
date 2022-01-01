90fc0aea360b6d9a9a8415dbf4f45a01e32ff2b7
jckrft <jckrft@gmail.com>
2021-12-31 19:50:04 -0500

    external links

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,25 +1,31 @@
-import '../assets/css/Contact.css';
+import "../assets/css/Contact.css";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export default function Contact() {
   return (
-    <div className="contact-links">
-      <a href="mailto:jckrft@gmail.com" target="_blank">
-        contact me | 
-      </a>
-      <a
-        className="external-link"
-        href="https://github.com/jckrft"
-        target="_blank"
-      >
-        github | 
+    <div className="contact">
+      <p className="connect">let's connect!</p>
+      <div className="contact-links">
+      <a className="external-link" href="mailto:jckrft@gmail.com" target="_blank">
+        <MailOutlineIcon />
       </a>
       <a
         className="external-link"
         href="https://www.linkedin.com/in/jckrft/"
         target="_blank"
       >
-        linkedin
+        <LinkedInIcon />
       </a>
+      <a
+        className="external-link"
+        href="https://github.com/jckrft"
+        target="_blank"
+      >
+        <GitHubIcon />
+        </a>
+        </div>
     </div>
   );
 }
