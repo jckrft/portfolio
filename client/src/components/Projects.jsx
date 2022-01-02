b77aec7408e4e62c8c3ad85ac450e2f9bd84f5e9
jckrft <jckrft@gmail.com>
2022-01-01 19:19:38 -0500

    project info

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -20,7 +20,7 @@ export default function Projects() {
             <text x="40" y="102">and adding to prompts. Only an</text>
             <text x="42" y="112">authenicated user can edit or</text>
             <text x="44" y="122">delete their own work. Built</text>
-            <text x="46" y="132">with React and Ruby on Rails</text>
+            <text x="46" y="132">with React and Ruby on Rails.</text>
             <a
               className="external-link"
               href="https://github.com/jckrft/what-is-your-story"
@@ -54,12 +54,12 @@ export default function Projects() {
             <text x="65" y="65" className="name-2">
               the library
             </text>
-            <text x="42" y="82">Full stack app that allows the user to</text>
-            <text x="40" y="92">practice creative writing by creating</text>
-            <text x="40" y="102">and adding to prompts. Only an</text>
-            <text x="42" y="112">authenicated user can edit or</text>
-            <text x="44" y="122">delete their own work. Built</text>
-            <text x="46" y="132">with React and Ruby on Rails</text>
+            <text x="55" y="82">An app that allows the user to</text>
+            <text x="48" y="92">browse through books. A user can</text>
+            <text x="42" y="102">create an account to add, edit, or</text>
+            <text x="42" y="112">delete any book. The Library is a full</text>
+            <text x="42" y="122">stack MERN app with built with a</text>
+            <text x="44" y="132">team of 4 developers using gitflow.</text>
             <a
               className="external-link"
               href="https://jasz-library.netlify.app"
@@ -89,9 +89,15 @@ export default function Projects() {
               d="M43.3,-37.3C53,-22.4,55.6,-4.5,53.9,16.1C52.3,36.8,46.3,60.3,31.9,68C17.4,75.6,-5.5,67.3,-24.6,56.2C-43.6,45,-58.8,31,-61.2,15.4C-63.6,-0.1,-53.1,-17,-40.7,-32.6C-28.4,-48.2,-14.2,-62.3,1.3,-63.4C16.8,-64.4,33.6,-52.3,43.3,-37.3Z"
               transform="translate(100 100)"
             />
-            <text x="68" y="70" className="name-3">
+            <text x="70" y="67" className="name-3">
               nether zone
             </text>
+            <text x="57" y="85">IYKYK. Fans of "Peep Show"</text>
+            <text x="52" y="95">can browse through famous</text>
+            <text x="46" y="105">quotes from the main character</text>
+            <text x="44" y="115">and add their own. Front-end app</text>
+            <text x="48" y="125">built using React and airtable.</text>
+            {/* <text x="44" y="132">team of 4 developers using gitflow.</text> */}
             <a
               className="external-link"
               href="https://netherzone.netlify.app"
